3e0d421218e6b922a848035c1c6990ccb5462d2f
kumar8317 <ankitkuma8317@gmail.com>
2024-06-11 12:20:37 +0530

    filter product

--- controllers/product.js
+++ controllers/product.js
@@ -7,7 +7,15 @@ import cloudinary from "cloudinary";
 
 export const getAllProducts = asyncError(async (req, res, next) => {
   //Search and category query
-  const products = await Product.find({});
+
+  const {keyword,category} = req.query
+  const products = await Product.find({
+    name:{
+      $regex: keyword?keyword:"",
+      $options: "i"
+    },
+    category: category? category: undefined
+  });
 
   res.status(200).json({
     success: true,
@@ -17,7 +25,7 @@ export const getAllProducts = asyncError(async (req, res, next) => {
 
 export const getProductDetails = asyncError(async (req, res, next) => {
   //Search and category query
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id).populate("category");
 
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
@@ -59,7 +67,7 @@ export const createProduct = asyncError(async (req, res, next) => {
 });
 
 export const updateProduct = asyncError(async (req, res, next) => {
-  const { name, description, price, stock } = req.body;
+  const { name, description, price, stock, category } = req.body;
 
   const product = await Product.findById(req.params.id);
 
@@ -70,6 +78,7 @@ export const updateProduct = asyncError(async (req, res, next) => {
   if (description) product.description = description;
   if (price) product.price = price;
   if (stock) product.stock = stock;
+  if(category) product.category = category;
 
   await product.save();
 
@@ -206,7 +215,7 @@ export const deleteCategory = asyncError(async (req, res, next) => {
 
 export const getAdminProducts = asyncError(async (req, res, next) => {
   //Search and category query
-  const products = await Product.find({});
+  const products = await Product.find({}).populate("category");
 
   res.status(200).json({
     success: true,
